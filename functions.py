7f30b51ec64faa6ff030e13b4505e2fd58f84954
ramzijebalia <jebaliaramzi@gmail.com>
2024-01-05 22:50:06 +0100

    create the detect object function using the DetrForObjectDetection model

--- functions.py
+++ functions.py
@@ -1,16 +1,19 @@
-from transformers import BlipProcessor , BlipForConditionalGeneration
+from transformers import BlipProcessor , BlipForConditionalGeneration , DetrImageProcessor, DetrForObjectDetection
 from PIL import Image
+import torch
 
 def get_image_caption(image_path):
+
+    # DEtection TRansformer (DETR) model LINK : https://huggingface.co/facebook/detr-resnet-50
     image = Image.open(image_path).convert('RGB')
     model_name = "Salesforce/blip-image-captioning-large"
     device = 'cpu'
 
     processor = BlipProcessor.from_pretrained(model_name)
     #processor = processor.to(device) 
-    model = BlipForConditionalGeneration.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
 
-    inputs = processor(image , return_tensors='pt')
+    inputs = processor(image , return_tensors='pt').to(device)
     output = model.generate(**inputs, max_new_tokens=20)
 
     caption = processor.decode(output[0] , skip_special_tokens=True)
@@ -18,10 +21,29 @@ def get_image_caption(image_path):
     return caption
 
 def detect_objects(image_path):
-    pass
+    image = Image.open(image_path).convert('RGB')
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+    
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {} , {}]  '.format(int(box[0]) , int(box[1]), int(box[2]), int(box[3]))
+        detections += '{}  '.format(model.config.id2label[int(label)])
+        detections += '{}\n'.format(float(score))
+    
+    return detections
 
 if __name__ =='__main__':
     image_path = "C:/Users/jebalia/Desktop/OIP.jpg"
-    caption = get_image_caption(image_path)
-    print(caption)
+    detections = detect_objects(image_path)
+    print(detections)
 
